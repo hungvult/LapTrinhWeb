ed7b1074571f7898e2cfd9dc609c4e1c972e3b18
hungtvlt <tuanhungvu0912@gmail.com>
2025-09-02 20:57:22 +0700

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LapTrinhWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
